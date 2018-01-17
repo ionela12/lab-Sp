b2c1d9235123b054e3dd61a42941a53372890900
ionela <ionela12@github.com>
2018-01-17 11:21:31 +0200

    laboratory

---
+++ src/com/fasterxml/jackson/databind/ObjectMapper.java
@@ -0,0 +1,18 @@
+package com.fasterxml.jackson.databind;
+
+import java.io.File;
+import java.util.HashMap;
+
+public class ObjectMapper {
+
+	public HashMap<String, Object> readValue1(File file, Class<HashMap> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public HashMap<String, Object> readValue(File file, Class<HashMap> class1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
